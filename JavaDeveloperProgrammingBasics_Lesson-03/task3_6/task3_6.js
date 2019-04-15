2032015a8a8cdd34e73b24ec282ee6b76b5c08c0
Alexey Dolgov <bifourcator@gmail.com>
2019-04-15 16:35:16 +0300

    Programming Basics Lesson 3 done!

---
+++ JavaDeveloperProgrammingBasics_Lesson-03/task3_6/task3_6.js
@@ -0,0 +1,24 @@
+﻿let browser = "IE";
+
+switch (browser) {
+    case "IE": 
+        console.log("IE is not supported");
+        break;
+    case "Chrome":
+    case "Firefox":
+    case "Safari":
+    case "Opera":
+        console.log("These browsers are supported");
+        break;
+    default:
+        console.log("We expect everything is OK in your browser!");
+}
+
+
+if ( browser === "IE" ) {
+        console.log("IE is not supported");
+} else if ( browser === "Chrome" || browser === "Firefox" || browser === "Safari" || browser === "Opera" ) {
+        console.log("These browsers are supported");
+} else {
+        console.log("We expect everything is OK in your browser!");
+}
